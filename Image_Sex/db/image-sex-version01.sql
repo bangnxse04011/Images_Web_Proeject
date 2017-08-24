e421fa7b6a5da5c85a177c74363d850e64bb7cdd
bangnx1 <bangnx1@fsoft.com.vn>
2017-08-24 09:13:40 +0700

    upadate database

--- Image_Sex/db/image-sex-version01.sql
+++ Image_Sex/db/image-sex-version01.sql
@@ -10,17 +10,88 @@ Target Server Type    : MYSQL
 Target Server Version : 50699
 File Encoding         : 65001
 
-Date: 2017-08-21 20:25:40
+Date: 2017-08-24 09:12:32
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for img_library
+-- ----------------------------
+DROP TABLE IF EXISTS `img_library`;
+CREATE TABLE `img_library` (
+`img_library_id`  int(11) NOT NULL ,
+`img_library_name`  varchar(1000) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
+`img_library_name_uuid`  varchar(45) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
+`img_librarycol`  varchar(45) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL 
+)
+ENGINE=InnoDB
+DEFAULT CHARACTER SET=latin1 COLLATE=latin1_swedish_ci
+
+;
+
+-- ----------------------------
+-- Records of img_library
+-- ----------------------------
+BEGIN;
+COMMIT;
+
+-- ----------------------------
+-- Table structure for img_thub
+-- ----------------------------
+DROP TABLE IF EXISTS `img_thub`;
+CREATE TABLE `img_thub` (
+`img_thub_id`  int(11) NOT NULL ,
+`img_thub_name`  varchar(45) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
+`img_thub_name_uuid`  varchar(45) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL 
+)
+ENGINE=InnoDB
+DEFAULT CHARACTER SET=latin1 COLLATE=latin1_swedish_ci
+
+;
+
+-- ----------------------------
+-- Records of img_thub
+-- ----------------------------
+BEGIN;
+COMMIT;
+
+-- ----------------------------
+-- Table structure for img_viewer
+-- ----------------------------
+DROP TABLE IF EXISTS `img_viewer`;
+CREATE TABLE `img_viewer` (
+`img_id`  int(11) NOT NULL AUTO_INCREMENT ,
+`img_name`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
+`img_link_avata`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
+`img_description`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
+`img_status`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
+`img_name_avata`  varchar(45) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
+`img_name_avata_uuid`  varchar(45) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
+`img_id_thumbnail`  int(11) NOT NULL ,
+`img_id_library`  int(11) NOT NULL ,
+`img_date_create`  date NOT NULL ,
+`img_money`  double NOT NULL ,
+PRIMARY KEY (`img_id`)
+)
+ENGINE=InnoDB
+DEFAULT CHARACTER SET=latin1 COLLATE=latin1_swedish_ci
+AUTO_INCREMENT=1
+
+;
+
+-- ----------------------------
+-- Records of img_viewer
+-- ----------------------------
+BEGIN;
+COMMIT;
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-`acc_id`  int(255) NOT NULL ,
+`acc_id`  int(255) NOT NULL AUTO_INCREMENT ,
 `acc_user`  varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
 `acc_pass`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
 `acc_first_fName`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
@@ -30,10 +101,12 @@ CREATE TABLE `user` (
 `acc_address`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
 `acc_money`  double NOT NULL ,
 `acc_status`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
+`acc_create`  date NOT NULL ,
 PRIMARY KEY (`acc_id`)
 )
 ENGINE=InnoDB
 DEFAULT CHARACTER SET=latin1 COLLATE=latin1_swedish_ci
+AUTO_INCREMENT=1
 
 ;
 
@@ -42,3 +115,13 @@ DEFAULT CHARACTER SET=latin1 COLLATE=latin1_swedish_ci
 -- ----------------------------
 BEGIN;
 COMMIT;
+
+-- ----------------------------
+-- Auto increment value for img_viewer
+-- ----------------------------
+ALTER TABLE `img_viewer` AUTO_INCREMENT=1;
+
+-- ----------------------------
+-- Auto increment value for user
+-- ----------------------------
+ALTER TABLE `user` AUTO_INCREMENT=1;
