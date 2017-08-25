4fed6d8e0d2833c2b0db58762b77687ada34454c
AnhNT <stork2312@gmail.com>
2017-08-25 20:13:41 +0700

    view detail image by id

--- Image_Sex/src/main/java/com/images/sex/entity/impl/ImgImpl.java
+++ Image_Sex/src/main/java/com/images/sex/entity/impl/ImgImpl.java
@@ -8,6 +8,9 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.dbutils.DbUtils;
+
+import com.images.sex.dto.ImageViewerDTO;
 import com.images.sex.entity.ImgViewer;
 import com.images.sex.entity.custom.ImagesCustom;
 import com.images.sex.entity.db.MysqlDataSource;
@@ -17,58 +20,172 @@ public class ImgImpl implements ImagesCustom, Serializable {
 	/**
 	 * 
 	 */
-	private static final long 			serialVersionUID = 1L;
-	private static StringBuilder 		stringBuilder 	= null;
-	private static Connection 			conn 			= null;
-	private static PreparedStatement 	ps 				= null;
-	private static ResultSet 			rs 				= null;
-	private static List<ImgViewer> 		listImgs		= null;
+	private static final long serialVersionUID = 1L;
+	private static StringBuilder stringBuilder = null;
+	private static Connection conn = null;
+	private static PreparedStatement ps = null;
+	private static ResultSet rs = null;
+	private static List<ImgViewer> listImgs = null;
 
 	/**
-	 * @throws SQLException 
+	 * @throws SQLException
 	 * 
 	 */
 	public Iterable<ImgViewer> findAllImg() throws SQLException {
-		//ImgStatus status;
+		// ImgStatus status;
 		ImgViewer imgViewer = new ImgViewer();
 		listImgs = new ArrayList<ImgViewer>();
 		conn = MysqlDataSource.getInstance().getConnect();
 		stringBuilder = new StringBuilder();
-		stringBuilder.append("SELECT i.* FROM img_viewer i ");
-//		stringBuilder.append("INNER JOIN img_thub it ");
-//		stringBuilder.append("ON i.img_id = it.img_thub_id ");
-//		stringBuilder.append("INNER JOIN img_library il ");
-//		stringBuilder.append("ON i.img_id = il.img_library_id ");
+		stringBuilder.append("SELECT * FROM img_viewer i ");
 		stringBuilder.append("WHERE i.img_status != 'BLOCKED' ");
 		ps = conn.prepareStatement(stringBuilder.toString());
-		//ps.setString(1, "BLOCKED");
 		System.out.println(stringBuilder.toString());
 		rs = ps.executeQuery();
-		while(rs.next()) {
+		while (rs.next()) {
 			Integer imgId = rs.getInt(1);
 			String imgName = rs.getString(2);
-			String imgLink = rs.getString(3);
+			String avatarLink = rs.getString(3);
 			String imgDes = rs.getString(4);
-			//String st = rs.getString(5);
-			//String name = rs.getString(6);
-			//String nameUUID = rs.getString(7);
-			//Integer imgIdThum = rs.getInt(8);
-			//Integer imgIdLibrary = rs.getInt(9);
-			//Date date = rs.getDate(10);
-			//Double money = rs.getDouble(11);
-			
-//			if(st.equals("ACTIVE")) {
-//				status = ImgStatus.ACTIVE;
-//			} else {
-//				status = ImgStatus.BLOCKED;
-//			}
+			// String st = rs.getString(5);
+			// String name = rs.getString(6);
+			// String nameUUID = rs.getString(7);
+			// Integer imgIdThum = rs.getInt(8);
+			// Integer imgIdLibrary = rs.getInt(9);
+			// Date date = rs.getDate(10);
+			// Double money = rs.getDouble(11);
+
+			// if(st.equals("ACTIVE")) {
+			// status = ImgStatus.ACTIVE;
+			// } else {
+			// status = ImgStatus.BLOCKED;
+			// }
 			imgViewer.setId(imgId);
-			imgViewer.setSourceAvata(imgLink);
+			imgViewer.setSourceAvata(avatarLink);
 			imgViewer.setDescription(imgDes);
 			imgViewer.setNameViewer(imgName);
 			listImgs.add(imgViewer);
 		}
+		DbUtils.closeQuietly(conn, ps, rs);
 		return listImgs;
+	}
+
+	/**
+	 * cần tạo thêm bảng img_library_viewer
+	 * 
+	 * @param id
+	 * @return
+	 * @throws SQLException
+	 */
+	public ImageViewerDTO findOne(Integer id) throws SQLException {
+		ImageViewerDTO imageViewerDTO = new ImageViewerDTO();
+		conn = MysqlDataSource.getInstance().getConnect();
+		stringBuilder = new StringBuilder();
+		stringBuilder.append("SELECT * FROM img_viewer i ");
+		stringBuilder.append("INNER JOIN img_library_viewer ilv ");
+		stringBuilder.append("ON i.img_id = ilv.img_id ");
+		stringBuilder.append("INNER JOIN img_library il ");
+		stringBuilder.append("ON ilv.img_library_id = il.img_library_id ");
+		stringBuilder.append("WHERE i.img_status != 'BLOCKED' AND i.img_id = ? ");
+		ps = conn.prepareStatement(stringBuilder.toString());
+		ps.setInt(1, id);
+		System.out.println(stringBuilder.toString());
+		rs = ps.executeQuery();
+		if (rs.next()) {
+			String imgName = rs.getString("img_name");
+			String avatarLink = rs.getString("img_link_avata");
+			String imgDes = rs.getString("img_description");
+			String linkDownload = rs.getString("img_library_name");
+			imageViewerDTO.setAlbumName(imgName);
+			imageViewerDTO.setDescription(imgDes);
+			imageViewerDTO.setLinkAvatar(avatarLink);
+			imageViewerDTO.setLinkDownload(linkDownload);
+			
+		}
+		DbUtils.closeQuietly(conn, ps, rs);
+		List<String> tags = findTagByAlbumId(id);
+		List<String> sampleImages = findSampleImageByAlbumId(id);
+		imageViewerDTO.setSampleImageLinks(sampleImages);
+		imageViewerDTO.setTags(tags);
+		return imageViewerDTO;
+	}
+
+	/**
+	 * cần tạo thêm bảng img_thub_viewer
+	 * 
+	 * @param id
+	 * @return
+	 * @throws SQLException
+	 */
+	public List<String> findSampleImageByAlbumId(Integer id) throws SQLException {
+		List<String> listSampleLinks = new ArrayList<String>();
+		conn = MysqlDataSource.getInstance().getConnect();
+		stringBuilder = new StringBuilder();
+		stringBuilder.append("SELECT * FROM img_viewer i ");
+		stringBuilder.append("INNER JOIN img_thub_viewer itv ");
+		stringBuilder.append("ON i.img_id = itv.img_id ");
+		stringBuilder.append("INNER JOIN img_thub it ");
+		stringBuilder.append("ON itv.img_thub_id = it.img_thub_id ");
+		stringBuilder.append("WHERE i.img_status != 'BLOCKED' AND i.img_id = ?");
+		ps = conn.prepareStatement(stringBuilder.toString());
+		ps.setInt(1, id);
+		rs = ps.executeQuery();
+		while (rs.next()) {
+			String name = rs.getString("img_thub_name_uuid");
+			listSampleLinks.add(name);
+		}
+		DbUtils.closeQuietly(conn, ps, rs);
+		return listSampleLinks;
+	}
+
+	/**
+	 * cần tạo thêm bảng img_tag_viewer
+	 * 
+	 * @param id
+	 * @return
+	 * @throws SQLException
+	 */
+	public List<String> findTagByAlbumId(Integer id) throws SQLException {
+		List<String> listTags = new ArrayList<String>();
+		conn = MysqlDataSource.getInstance().getConnect();
+		stringBuilder = new StringBuilder();
+		stringBuilder.append("SELECT * FROM img_viewer i ");
+		stringBuilder.append("INNER JOIN img_tag_viewer itv ");
+		stringBuilder.append("ON i.img_id = itv.img_viewer_id ");
+		stringBuilder.append("INNER JOIN img_tag it ");
+		stringBuilder.append("ON itv.img_tag_id = it.img_tag_id ");
+		stringBuilder.append("WHERE i.img_status != 'BLOCKED' AND i.img_id = ? ");
+		ps = conn.prepareStatement(stringBuilder.toString());
+		ps.setInt(1, id);
+		rs = ps.executeQuery();
+		while (rs.next()) {
+			String name = rs.getString("img_tag_name");
+			listTags.add(name);
+		}
+		DbUtils.closeQuietly(conn, ps, rs);
+		return listTags;
+	}
+
+	public static void main(String args[]) {
+		try {
+			ImageViewerDTO dto = new ImgImpl().findOne(1);
+			System.out.println(dto.getAlbumName());
+			System.out.println(dto.getDescription());
+			System.out.println(dto.getLinkAvatar());
+			System.out.println(dto.getLinkDownload());
+			List<String> tags = dto.getTags();
+			for (String string : tags) {
+				System.out.print(string + "\t");
+			}
+			System.out.println("\n");
+			List<String> samples = dto.getSampleImageLinks();
+			for (String string : samples) {
+				System.out.print(string + "\t");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
